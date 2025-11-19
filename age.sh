c0555c3bbba6bf54f0d6c72117beeeaa7fa447b1
yolande oben <obenyolande@gmail.com>
2025-11-19 15:46:32 +0100

    i modified age.sh script

--- age.sh
+++ age.sh
@@ -5,11 +5,11 @@ read -p "Enter your name; " name
 read -p "Enter your age;  " age
 
 if [[ $age -lt 10 ]]; then
-echo "$name is the age group of child"
+echo "$name is the age group of todlers"
 elif [[ $age  -gt 10 && $age -lt 20 ]]; then
 echo "$name is in the age group of teenagers"
 elif [[ $age -gt 20 && $age -lt 30 ]]; then
 echo $name is in the age group of adult
 else
-echo "$name is OLD"
+echo "$name is people of diex age"
 fi
